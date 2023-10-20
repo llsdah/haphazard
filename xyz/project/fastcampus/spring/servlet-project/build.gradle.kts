7dc87384e9c147d0ad6e580331025e5849ed97a4
llsdah <77381506+llsdah@users.noreply.github.com>
2023-10-20 15:19:28 +0900

    servlet 1

--- xyz/project/fastcampus/spring/servlet-project/build.gradle.kts
+++ xyz/project/fastcampus/spring/servlet-project/build.gradle.kts
@@ -10,12 +10,16 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.11")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 
-    testImplementation("org.assertj:assertj-core:3.23.1")
+    // embedded tomcat
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:8.5.42")
+    implementation ("org.apache.tomcat.embed:tomcat-embed-jasper:8.5.42")
 
+    // logback-classic
+    implementation ("ch.qos.logback:logback-classic:1.2.3")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
--- xyz/project/fastcampus/spring/servlet-project/src/main/java/org/example/calculator/domain/Calculator.java
+++ xyz/project/fastcampus/spring/servlet-project/src/main/java/org/example/calculator/domain/Calculator.java
@@ -1,18 +1,20 @@
 package org.example.calculator.domain;
 
 import org.example.calculator.tobe.*;
-
 import java.util.List;
 
 public class Calculator {
 
     private static final List<ArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new DivisionOperator(), new MultipleOperator(),new SubtractionOperator());
+
     /**
      * 간단한 사칙연산을 할 수 있다.
      * 양수로만 계산할 수 있다.
      * 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다. MVC패턴(Model-View-Controller) 기반으로 구현한다
      */
+
     public static int calculate(PositiveNumber positiveOperand1, String operator, PositiveNumber positiveOperand2) {
+
         try {
             Thread.sleep(5_000L);
         } catch (InterruptedException e) {
