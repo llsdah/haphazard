b4531adc95e904e0b2e497136bd7ad339ad8e2bf
llsdah <77381506+llsdah@users.noreply.github.com>
2023-10-24 21:18:21 +0900

    servlet 초기 버전 구현

--- xyz/project/fastcampus/spring/servlet-project/src/main/java/org/example/CalulatorServlet.java
+++ xyz/project/fastcampus/spring/servlet-project/src/main/java/org/example/CalulatorServlet.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/calculate")
@@ -18,8 +21,17 @@ public class CalulatorServlet implements Servlet {
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         logger.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+        String operator = request.getParameter("operator");
+
+        int result = Calculator.calculate(new PositiveNumber(operand1),operator,new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
+
     }
 
     @Override
